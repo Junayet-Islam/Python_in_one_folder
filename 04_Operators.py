a6faf0efcbefcd240cf75b71b38e8f7a9948415e
Junayet-Islam <junayetislam100@gmail.com>
2021-12-14 16:09:10 +0600

    Fully modified operator.py

--- 04_Operators.py
+++ 04_Operators.py
@@ -1,8 +1,21 @@
 # we have various types of operators......such as arithmetic, logical, bitwise, comparison etc....
 # we can have same sign to use as operators in muliple scenarios
+
+
+
+
+
+
+
+
+
 #----------------------------------------------------------------
 # 01. Arithmetic Operators
 #----------------------------------------------------------------
+
+
+
+
 # in  arithmetic operators, we use these operators to do maths
 
 # (+)----addition-----x+y 
@@ -14,21 +27,93 @@
 # (//)----Floor Division----- x//y 
 
 
-a = 10
-b = 20
+# a = 10
+# b = 20
 
-print (a + b)
-print (a - b)
-print (a * b)
-print (a / b)
+# print (a + b)
+# print (a - b)
+# print (a * b)
+# print (a / b)
 
-c = 10
-d = 3
-print (c % d)# this is called reminder.......after deviding the numbers...the rest or so called reminder will print
+# c = 10
+# d = 3
+# print (c % d)# this is called reminder.......after deviding the numbers...the rest or so called reminder will print
 
-print (c ** d) # this is called the basic power function...like 5**2 is the 5*5 or 5**3 is the 5*5*5
+# print (c ** d) # this is called the basic power function...like 5**2 is the 5*5 or 5**3 is the 5*5*5
 
-print (c // d) # this gives the division also but don't give the numbers of the decimal....like if the division comes 3.33, it will give 3, the rest will be poped out...so it ususally give the integer number
+# print (c // d) # this gives the division also but don't give the numbers of the decimal....like if the division comes 3.33, it will give 3, the rest will be poped out...so it ususally give the integer number
 
 
 
+#----------------------------------------------------------------
+# 02 . Assignment Operators
+#----------------------------------------------------------------
+
+# = -> x = 5 -> same as x = 5
+# += -> x += 5 -> same as x = x + 5
+# # -= -> x -= 5 -> same as x = x - 5 
+
+# x = 6
+# print(x)     
+# x+=5
+# print(x)
+# x-=5
+# print(x)
+
+
+
+
+#----------------------------------------------------------------
+# 03 . Comparision Operators
+#----------------------------------------------------------------
+
+# it mainly use to compare two conditions
+
+# == means equal to
+# != means not equal to
+# > greater than 
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+
+# x = 10
+# y = 20
+
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+
+#----------------------------------------------------------------
+# 03 . logical operator Operators
+#----------------------------------------------------------------
+
+# we use this operator to use the extended version of comparison operators
+# when we get more than two conditions to check at once....we use logical operator
+
+# and -> return True if all or both statements are correct-----
+# or -> returns True if one of the statements are correct....
+# not -> reverse the result what it gets from the condition 
+
+# a = 10
+# b = 20
+
+# print(x==10 and x<y)
+# print(x==10 or x<y)
+# print(x==10 or x<y)
+
+#----------------------------------------------------------------
+# 04 . Bitwise Operators
+#----------------------------------------------------------------
+
+
+# here we use the binary method to match the conditions to true and False
+
+# if it returns 1, this means True or if it returns 0 then it indicates false
+
+# & -> and 
+# | -> or
+# ^ -> xor
